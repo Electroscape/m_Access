2d12a42afdd698fae3b2012b69679b598a233bcb
Seif elbouhy <seif.elbouhy@gmail.com>
2022-08-16 15:46:34 +0200

    Test Keypad code
    receive input from keypad to brain

--- src/m_Acces.ino
+++ src/m_Acces.ino
@@ -1,8 +1,16 @@
 #include "header.h"
 #include "header_st.h"
+#include "header_s.h"
+using namespace stb_namespace;
+#include <Keypad.h> /* Standardbibliothek                                                 */
+#include <Keypad_I2C.h>
+#include "PCF8574.h"
 #include <stb_common.h>
 #include <stb_brain.h>
 #include <avr/wdt.h>
+#include <PCF8574.h> /* https://github.com/skywodd/pcf8574_arduino_library - modifiziert!  */
+
+#include <Password.h>
 #include <stb_oled.h>
 #include <stb_rfid.h>
 
@@ -20,12 +28,90 @@ STB_BRAIN BRAIN;
     unsigned long lastRfidCheck = millis();
 #endif
 
+/*==KEYPAD I2C============================================================*/
+const byte KEYPAD_ROWS = 4;  // Zeilen
+const byte KEYPAD_COLS = 3;  // Spalten
+const byte KEYPAD_CODE_LENGTH = 4;
+const byte KEYPAD_CODE_LENGTH_MAX = 7;
+
+char KeypadKeys[KEYPAD_ROWS][KEYPAD_COLS] = {
+    {'1', '2', '3'},
+    {'4', '5', '6'},
+    {'7', '8', '9'},
+    {'*', '0', '#'}};
+
+
+byte KeypadRowPins[KEYPAD_ROWS] = {1, 6, 5, 3};  // Zeilen  - Messleitungen
+byte KeypadColPins[KEYPAD_COLS] = {2, 0, 4};     // Spalten - Steuerleitungen (abwechselnd HIGH)
+
+static unsigned long update_timer = millis();
+const int keypad_reset_interval = 3000;
+
+Keypad_I2C Keypad(makeKeymap(KeypadKeys), KeypadRowPins, KeypadColPins, KEYPAD_ROWS, KEYPAD_COLS, KEYPAD_ADD, PCF8574_MODE);
+
+// Passwort
+Password passKeypad = Password(secret_password);
+
+/*==PCF8574============================================================*/
+PCF8574 relay;
+
+
+bool relay_init() {
+    Serial.println("initializing relay");
+    relay.begin(RELAY_I2C_ADD);
+
+    for (int i = 0; i < REL_AMOUNT; i++) {
+        relay.pinMode(relayPinArray[i], OUTPUT);
+        relay.digitalWrite(relayPinArray[i], relayInitArray[i]);
+    }
+
+    Serial.print(F("\n successfully initialized relay\n"));
+    return true;
+}
+
+bool keypad_init() {
+    Keypad.addEventListener(keypadEvent);  // Event Listener erstellen
+    Keypad.begin(makeKeymap(KeypadKeys));
+    Keypad.setHoldTime(5000);
+    Keypad.setDebounceTime(20);
+    return true;
+}
+
+void keypadEvent(KeypadEvent eKey) {
+    KeyState state = IDLE;
+
+    state = Keypad.getState();
+
+    switch (state) {
+        case PRESSED:
+            update_timer = millis();
+            switch (eKey) {
+                case '#':
+                    STB.dbgln("change pass");
+                    break;
+
+                case '*':
+                    STB.dbgln("reset pass");
+                    break;
+
+                default:
+                    passKeypad.append(eKey);
+                   // printWithHeader(passKeypad.guess, relayCode);
+                    break;
+            }
+            break;
+
+        default:
+            break;
+    }
+}
+
 void setup() {
     
     // starts serial and default oled
     
     STB.begin();
-    STB.serialInit();
+   
     STB.i2cScanner();
     
     Serial.println("WDT endabled");
@@ -44,6 +130,22 @@ void setup() {
         // led init
     //}
     pinMode(buzzer,OUTPUT);
+
+    STB.dbgln("Relay: ...");
+    if (relay_init()) {
+        STB.dbgln("ok");
+    }
+    wdt_reset();
+
+    //delay(50);
+
+    STB.dbgln("Keypad: ...");
+    if (keypad_init()) {
+        STB.dbgln(" ok\n");
+    }
+    wdt_reset();
+    delay(5);
+
     
     
     STB.printSetupEnd();
@@ -57,17 +159,18 @@ void setup() {
 void loop() {
 
     wdt_reset();
-    #ifndef rfidDisable
-        rfidRead();
-    #endif
-    delay(1000);
-
-    tone(buzzer,1700);
-    STB.dbgln("peep for 300ms");
-    delay(300);
-    noTone(buzzer);
-    STB.dbgln("stop for 200ms");
-    delay(200);
+    Keypad.getKey();
+   // #ifndef rfidDisable
+   //     rfidRead();
+   // #endif
+   // delay(1000);
+//
+   // tone(buzzer,1700);
+   // STB.dbgln("peep for 300ms");
+   // delay(300);
+   // noTone(buzzer);
+   // STB.dbgln("stop for 200ms");
+   // delay(200);
     
     //STB.rs485AddToBuffer("my response/data");
     // sends out buffer once polled along with !eof, to handle cmds be aware this sends out the !EOF
