4da001b7b529101d7adbeea925524c3c0aa296a9
MartinPek <martin_pek@web.de>
2022-09-09 14:00:53 +0200

    motherSkeleton: added password/rfid list & mapping to stages

--- motherSkeleton/include/header_st.h
+++ motherSkeleton/include/header_st.h
@@ -1,1 +1,24 @@
 #pragma once
+
+#define StageCount 10
+#define passCnt 4
+#define maxPassLen 10
+
+char passwords[passCnt][maxPassLen] = {
+    "1234",
+    "4321",
+    "1337",
+    "0000"
+};
+
+
+// defines what password/RFIDCode is used at what stage, if none is used its -1
+int passwordMap[StageCount] = {
+    0,
+    1,
+    2,
+    3,
+    -1,
+    -1,
+    -1
+};
